86cb6d0b7e31146b0870e225493111152d9c9dab
Ugo-Larsonneur <ugo.larsonneur@etu.univ-amu.fr>
2021-01-14 12:11:35 +0100

    Ajout de la base

---
+++ base/simp-land_db(1).sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 14, 2021 at 11:09 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `simp-land_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emoji`
+--
+
+CREATE TABLE `emoji` (
+  `id_user` int(11) NOT NULL,
+  `id_message` int(11) NOT NULL,
+  `emoji_name` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emoji_count`
+--
+
+CREATE TABLE `emoji_count` (
+  `id_message` int(11) NOT NULL,
+  `nom_emoji` varchar(20) NOT NULL,
+  `quantite` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `id_Message` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `contenu` varchar(50) NOT NULL,
+  `photo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `pseudo` varchar(20) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `mdp` varchar(128) NOT NULL,
+  `role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `pseudo`, `email`, `mdp`, `role`) VALUES
+(1, 'Vanestarre', 'vanestarre@vanestarre.vanestarre', 'vanestarre', 'SUPER_ADMIN'),
+(2, 'Vanefan', 'vanefan@gmail.com', 'vanefan', 'MEMBER');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `emoji`
+--
+ALTER TABLE `emoji`
+  ADD PRIMARY KEY (`id_user`,`id_message`,`emoji_name`);
+
+--
+-- Indexes for table `emoji_count`
+--
+ALTER TABLE `emoji_count`
+  ADD PRIMARY KEY (`id_message`,`nom_emoji`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id_Message`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id_Message` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
