8372a24fba748a6daa98d4baeddf4971de975ea9
Gaetan <gaetan.trinca-pupet@etu.univ-amu.fr>
2021-01-15 21:13:06 +0100

    Complétion de create_user

---
+++ Site/php/create_user.php
@@ -0,0 +1,32 @@
+<?php
+    /// Include - Require
+    require '../include/part.php';
+?>
+
+<?php page_head('Se connecter','main.css'); ?>
+
+
+<body>
+    <?php page_header(); ?>
+
+    <div id="center_square">
+        <form action="pass_verify.php" method="post">
+            <p><label for="pseudo">Pseudo :</label></br>
+                <input type="text" name="pseudo" placeholder="Pseudo" required></p>
+
+            <p><label for="mail">E-mail :</label></br>
+                <input type="text" name="mail" placeholder="E-mail" required></p>
+
+            <p><label for="password">Mot de passe :</label></br>
+                <input type="password" name="password" placeholder="Mot de passe" required></p>
+
+            <p><label for="passwordbis">Rentrez votre mot de passe une seconde fois :</label></br>
+                <input type="password" name="passwordbis" placeholder="Mot de passe" required></p>
+
+            <p><label for="conditions">Acceptez les conditions d'utilisations : </label><input type="checkbox" name="conditions" required></p>
+
+            <p><input type="submit" name="action" id="soumettre" value="Soumettre"></p>
+        </form>
+    </div>
+</body>
+
