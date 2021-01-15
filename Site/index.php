aa7438cb70ef2300cf034587de4f1dd278586a7f
Gaetan <gaetan.trinca-pupet@etu.univ-amu.fr>
2021-01-15 21:11:31 +0100

    MAJ index
    ajout d'un lien vers la page de création de compte

--- Site/index.php
+++ Site/index.php
@@ -2,5 +2,6 @@
     echo'<h1>Bonjour</h1>';
     echo'<a href="php/acceuil.php">Page d\'acceuil</a></br>
     <a href="php/connexion.php">Page de connexion</a></br>
+    <a href="php/create_user.php">Page de création de compte</a></br>
     ';
 ?>
