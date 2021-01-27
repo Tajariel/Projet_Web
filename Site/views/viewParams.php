45c3fd6c0f6c23cb6880e1253272de49b90ad936
Gaetan <gaetan.trinca-pupet@etu.univ-amu.fr>
2021-01-27 18:21:23 +0100

    viewParam MAJ

--- Site/views/viewParams.php
+++ Site/views/viewParams.php
@@ -8,7 +8,26 @@ class viewParams extends view
     {
 
         echo '
-        
+        <div id="page">
+        <div id="center_square">
+            <form action="controllers/Router.php" method="get">
+                <p><label for="pseudo">Pseudo</label></br>
+                    <input class="intext" type="text" name="pseudo" placeholder="Pseudo" required></p>
+
+                <p><label for="email">E-mail</label></br>
+                    <input class="intext" type="text" name="email" placeholder="E-mail" required></p>
+
+                <p><label for="password">Mot de passe</label></br>
+                    <input class="intext" type="password" name="password" placeholder="Mot de passe" required></p>
+
+                <p><label for="passwordbis">Rentrez votre mot de passe une seconde fois</label></br>
+                    <input class="intext" type="password" name="passwordbis" placeholder="Mot de passe" required></p>
+                <p><label for="conditions">Acceptez les conditions d\'utilisations : <?php customCheckBox("conditions",true)?></label></p>
+
+            <p><input class="button" type="submit" name="action" id="soumettre" value="creation"></p>
+            </form>
+        </div>
+    </div>';
         ';
     }
 }
