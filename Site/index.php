079cccdb28563a4ae47a5c6cc33130897e0de2a1
Gaetan <gaetan.trinca-pupet@etu.univ-amu.fr>
2021-01-27 16:30:11 +0100

    MAJ index

--- Site/index.php
+++ Site/index.php
@@ -6,9 +6,9 @@ include_once 'controllers/Router.php';
 if (!isset($_POST['redirection'])) {
     $router = new Router();
 }
-echo $_POST['action'];
-echo $_POST['pseudo'];
-echo $_POST['password'];
+//echo $_POST['action'];
+//echo $_POST['pseudo'];
+//echo $_POST['password'];
 
 $router->routeReq();
 
