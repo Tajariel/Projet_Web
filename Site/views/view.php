c87992c83260916df47658c979c31af495deb027
tajariel <marc.ambaud@etu.univ-amu.fr>
2021-01-28 21:17:58 +0100

    redirection alwaysdata supprimé+Déconnexion fixed

--- Site/views/view.php
+++ Site/views/view.php
@@ -52,7 +52,7 @@ abstract class view
             echo '
                         <form method="post"><button class="button" type="submit" name="action" value="parametre">Parametre</button></form>
                         <label>'.$_SESSION['user']['pseudo'].'</label>
-                        <form method="post"><input class="button" type="submit" name="action" value="Déconexion"></form>
+                        <form method="post"><input class="button" type="submit" name="action" value="Déconnexion"></form>
             ';
         }
         echo '
