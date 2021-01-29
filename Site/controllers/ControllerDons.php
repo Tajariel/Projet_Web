9cd939b2411cd89caf7a7884de36e76a017e4873
FufuM <mfurter.alphand@gmail.com>
2021-01-29 02:47:06 +0100

    Implementation dons

--- Site/controllers/ControllerDons.php
+++ Site/controllers/ControllerDons.php
@@ -14,5 +14,11 @@ class ControllerDons
     private function Dons()
     {
         $this->_view = new viewDons();
+        $titre = 'Dons';
+
+        $this->_view->echoHead($titre);
+        $this->_view->echoHeader();
+        $this->_view->echoPaypal();
+        $this->_view->echoTail();
     }
 }
--- Site/views/viewDons.php
+++ Site/views/viewDons.php
@@ -5,6 +5,15 @@ class viewDons extends view
 {
     public function echoPaypal()
     {
-
+    echo '        <div id="page">
+            <div id="center_square">
+                <form method="post">
+                    <form action="https://www.paypal.com/donate" method="post" target="_top">
+    <input type="hidden" name="hosted_button_id" value="DV5LZ4AYQSC3G" />
+    <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+    <img alt="" border="0" src="https://www.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1" />
+</form>
+        </div>
+        </div>';;
     }
 }
